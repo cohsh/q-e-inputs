3b17cd804516a735d88bc3a70413b2bc6fa4c4c3
cohsh <82075327+cohsh@users.noreply.github.com>
2023-02-13 13:48:44 +0900

    [add] utils/ and generate_jobscript.py

---
+++ generate_jobscript.py
@@ -0,0 +1,31 @@
+from qeinput.material import Material
+from qeinput.inputs import SlurmJob, InputPWSCF, InputPWNSCF
+
+
+def main():
+    pseudo_dir = "./pseudos/local"
+    job = SlurmJob("PartitionName", 1, 128, 1)
+
+    key = "Your API key of the Materials Project"
+
+    Si = Material(key, "mp-149")
+    prefix = Si.formula_pretty
+
+    Si_input_scf = InputPWSCF(Si, pseudo_dir, 60, [8, 8, 8])
+    Si_input_nscf = InputPWNSCF(Si, pseudo_dir, 60,
+                                [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]], 10)
+
+    Si_inputs = {"scf": Si_input_scf, "nscf": Si_input_nscf}
+
+    for calc, Si_input in Si_inputs.items():
+        infile = prefix + "." + calc + ".in"
+        outfile = prefix + "." + calc + ".out"
+        Si_input.generate(infile)
+
+        job.add_srun("pw.x", "", infile, outfile)
+
+    job.generate("job.sh")
+
+
+if __name__ == "__main__":
+    main()
